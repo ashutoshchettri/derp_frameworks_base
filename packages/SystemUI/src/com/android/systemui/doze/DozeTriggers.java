LMODroid/platform_frameworks_base

base: Add ambient pulse notification
Squashed:
- base: Add ambient pulse notification [1/2]
- SystemUI: Allow changing duration of ambient edge light [1/2]
- SystemUI: Add auto color option for edge lightning [1/2]
- SystemUI: Allow limiting edge lighting repeats [1/2]
- SystemUI: Improve edge lighting color modes
- NotificationLights: enable turning off repeat
- SystemUI: Add light width [1/3]
- SystemUI: NotificationLightsView: Silence logs
- NotificationLightsView: Remove unused code
- SystemUI: Use notification icon color for ambient edge light
- SystemUI: Consider ambient pulse lights duration for headups display time
- SystemUI: Animate edge light only for notification pulse
- SystemUI: Add support to animate edge light only when device face down
- SystemUI: Rewrite pulse light view in kotlin
- PulseLight: Use ContentObserver instead of TunerService

Change-Id: I18895817f0d74321d112e6ca1e3ab1135ff52bd0
Co-authored-by: kldoc <fred.berbigier@gmail.com>

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -29,6 +29,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.SystemClock;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.format.Formatter;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
@@ -40,6 +42,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
+import com.android.systemui.R;
 import com.android.systemui.biometrics.AuthController;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dock.DockManager;
@@ -56,6 +59,8 @@ import com.android.systemui.util.sensors.ProximityCheck;
 import com.android.systemui.util.sensors.ProximitySensor;
 import com.android.systemui.util.settings.SecureSettings;
 import com.android.systemui.util.wakelock.WakeLock;
+
+import com.libremobileos.providers.LMOSettings;
 
 import java.io.PrintWriter;
 import java.util.Optional;
@@ -188,6 +193,8 @@ public class DozeTriggers implements DozeMachine.Part {
         }
     }
 
+    private boolean mPulseLightOnFaceDown = false;
+
     @Inject
     public DozeTriggers(Context context, DozeHost dozeHost,
             AmbientDisplayConfiguration config,
@@ -222,6 +229,9 @@ public class DozeTriggers implements DozeMachine.Part {
         mUiEventLogger = uiEventLogger;
         mKeyguardStateController = keyguardStateController;
         mUserTracker = userTracker;
+
+        mPulseLightOnFaceDown = mContext.getResources()
+                .getBoolean(R.bool.config_showEdgeLightOnlyWhenFaceDown);
     }
 
     @Override
@@ -572,7 +582,17 @@ public class DozeTriggers implements DozeMachine.Part {
 
         mDozeHost.setPulsePending(true);
         proximityCheckThenCall((isNear) -> {
-            if (isNear != null && isNear) {
+            // Don't skip pulse for notifications when proximity is near
+            // if edge light is enabled for face down only.
+            boolean shouldPulse = false;
+            if (mPulseLightOnFaceDown) {
+                boolean edgeLightEnabled = Settings.Secure.getIntForUser(
+                        mContext.getContentResolver(), LMOSettings.Secure.PULSE_AMBIENT_LIGHT,
+                        0, UserHandle.USER_CURRENT) != 0;
+                boolean pulseForNotification = reason == DozeLog.PULSE_REASON_NOTIFICATION;
+                shouldPulse = edgeLightEnabled && pulseForNotification;
+            }
+            if (isNear != null && isNear && !shouldPulse) {
                 // in pocket, abort pulse
                 mDozeLog.tracePulseDropped("requestPulse - inPocket");
                 mDozeHost.setPulsePending(false);

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -66,6 +66,7 @@ import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Process;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.VibrationEffect;
 import android.provider.Settings;
@@ -152,6 +153,7 @@ import com.android.systemui.keyguard.ui.viewmodel.LockscreenToDreamingTransition
 import com.android.systemui.keyguard.ui.viewmodel.LockscreenToOccludedTransitionViewModel;
 import com.android.systemui.keyguard.ui.viewmodel.OccludedToLockscreenTransitionViewModel;
 import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToGoneTransitionViewModel;
+import com.android.systemui.libremobileos.pulselight.PulseLightView;
 import com.android.systemui.media.controls.pipeline.MediaDataManager;
 import com.android.systemui.media.controls.ui.KeyguardMediaController;
 import com.android.systemui.media.controls.ui.MediaHierarchyManager;
@@ -186,6 +188,7 @@ import com.android.systemui.statusbar.notification.NotificationWakeUpCoordinator
 import com.android.systemui.statusbar.notification.PropertyAnimator;
 import com.android.systemui.statusbar.notification.ViewGroupFadeHelper;
 import com.android.systemui.statusbar.notification.collection.NotificationEntry;
+import com.android.systemui.statusbar.notification.collection.NotifPipeline;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.notification.row.ExpandableView;
 import com.android.systemui.statusbar.notification.row.NotificationGutsManager;
@@ -695,6 +698,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     private final ActivityStarter mActivityStarter;
 
+    private PulseLightView mPulseLightView;
+    private NotifPipeline mNotifPipeline;
+
     @Inject
     public NotificationPanelViewController(NotificationPanelView view,
             @Main Handler handler,
@@ -782,7 +788,8 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             KeyguardViewConfigurator keyguardViewConfigurator,
             KeyguardFaceAuthInteractor keyguardFaceAuthInteractor,
             KeyguardRootView keyguardRootView,
-            Context context) {
+            Context context,
+            NotifPipeline notifPipeline) {
         keyguardStateController.addCallback(new KeyguardStateController.Callback() {
             @Override
             public void onKeyguardFadingAwayChanged() {
@@ -1006,6 +1013,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mAlternateBouncerInteractor = alternateBouncerInteractor;
         mKeyguardRootView = keyguardRootView;
         dumpManager.registerDumpable(this);
+        mNotifPipeline = notifPipeline;
     }
 
     private void unlockAnimationFinished() {
@@ -1096,6 +1104,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (!mFeatureFlags.isEnabled(Flags.MIGRATE_SPLIT_KEYGUARD_BOTTOM_AREA)) {
             setKeyguardBottomArea(mView.findViewById(R.id.keyguard_bottom_area));
         }
+        mPulseLightView = (PulseLightView) mView.findViewById(R.id.pulse_light_view);
 
         initBottomArea();
 
@@ -3192,9 +3201,42 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (!mPulsing && !mDozing) {
             mAnimateNextPositionUpdate = false;
         }
+
+        showPulseLight();
+
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
 
         updateKeyguardStatusViewAlignment(/* animate= */ true);
+    }
+
+    private void showPulseLight() {
+        if (mPulseLightView == null || !isPulseLightEnabled()) return;
+        if (mPulsing) {
+            // Get the notification that's pulsing
+            String notifPackageName = "";
+            List<NotificationEntry> notificationEntries =
+                    new ArrayList(mNotifPipeline.getAllNotifs());
+            for (int i = 0; i < notificationEntries.size(); i++) {
+                NotificationEntry entry = notificationEntries.get(i);
+                if (entry.showingPulseLight()) {
+                    notifPackageName = entry.getSbn().getPackageName();
+                    break;
+                }
+            }
+            // Animate edge light only for notification pulse.
+            // Package not empty means pulse caused by a notification.
+            if (!notifPackageName.isEmpty()) {
+                mPulseLightView.setVisibility(View.VISIBLE);
+                mPulseLightView.animateNotification(notifPackageName);
+            }
+        } else {
+            mPulseLightView.setVisibility(View.GONE);
+        }
+    }
+
+    private boolean isPulseLightEnabled() {
+        return Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
+                LMOSettings.Secure.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/statusbar/notification/collection/coordinator/PulseLightCoordinator.kt
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2023 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.statusbar.notification.collection.coordinator
+
+import android.app.Notification
+
+import com.android.systemui.libremobileos.pulselight.PulseLightNotifManager
+import com.android.systemui.statusbar.notification.collection.NotifPipeline
+import com.android.systemui.statusbar.notification.collection.NotificationEntry
+import com.android.systemui.statusbar.notification.collection.coordinator.dagger.CoordinatorScope
+import com.android.systemui.statusbar.notification.collection.notifcollection.NotifCollectionListener
+
+import javax.inject.Inject
+
+@CoordinatorScope
+class PulseLightCoordinator @Inject constructor(
+    private val manager: PulseLightNotifManager
+) : Coordinator {
+
+    private val notifCollectionListener = object : NotifCollectionListener {
+
+        override fun onEntryAdded(entry: NotificationEntry) {
+            manager.onNotificationPosted(entry)
+        }
+
+        override fun onEntryUpdated(entry: NotificationEntry) {
+            // Pass notification only if it's have to alert user again.
+            if (shouldAlertAgain(entry)) {
+                manager.onNotificationPosted(entry)
+            }
+        }
+
+    }
+
+    /**
+     * Checks whether an update for a notification warrants an alert for the user.
+     */
+    private fun shouldAlertAgain(entry: NotificationEntry): Boolean {
+        return (!entry.hasInterrupted() ||
+                (entry.sbn.notification.flags and Notification.FLAG_ONLY_ALERT_ONCE) == 0)
+    }
+
+
+    override fun attach(pipeline: NotifPipeline) {
+        pipeline.addCollectionListener(notifCollectionListener)
+    }
+
+}

ashutoshchettri/derp_frameworks_base

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -29,6 +29,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.SystemClock;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.format.Formatter;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
@@ -40,6 +42,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
+import com.android.systemui.R;
 import com.android.systemui.biometrics.AuthController;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dock.DockManager;
@@ -188,6 +191,8 @@ public class DozeTriggers implements DozeMachine.Part {
         }
     }
 
+    private boolean mPulseLightOnFaceDown = false;
+
     @Inject
     public DozeTriggers(Context context, DozeHost dozeHost,
             AmbientDisplayConfiguration config,
@@ -222,6 +227,8 @@ public class DozeTriggers implements DozeMachine.Part {
         mUiEventLogger = uiEventLogger;
         mKeyguardStateController = keyguardStateController;
         mUserTracker = userTracker;
+        mPulseLightOnFaceDown = mContext.getResources()
+                .getBoolean(R.bool.config_showEdgeLightOnlyWhenFaceDown);
     }
 
     @Override
@@ -572,7 +579,17 @@ public class DozeTriggers implements DozeMachine.Part {
 
         mDozeHost.setPulsePending(true);
         proximityCheckThenCall((isNear) -> {
-            if (isNear != null && isNear) {
+            // Don't skip pulse for notifications when proximity is near
+            // if edge light is enabled for face down only.
+            boolean shouldPulse = false;
+            if (mPulseLightOnFaceDown) {
+                boolean edgeLightEnabled = Settings.Secure.getIntForUser(
+                        mContext.getContentResolver(), Settings.Secure.PULSE_AMBIENT_LIGHT,
+                        0, UserHandle.USER_CURRENT) != 0;
+                boolean pulseForNotification = reason == DozeLog.PULSE_REASON_NOTIFICATION;
+                shouldPulse = edgeLightEnabled && pulseForNotification;
+            }
+            if (isNear != null && isNear && !shouldPulse) {
                 // in pocket, abort pulse
                 mDozeLog.tracePulseDropped("requestPulse - inPocket");
                 mDozeHost.setPulsePending(false);

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -172,6 +172,7 @@ import com.android.systemui.keyguard.ui.viewmodel.LockscreenToDreamingTransition
 import com.android.systemui.keyguard.ui.viewmodel.LockscreenToOccludedTransitionViewModel;
 import com.android.systemui.keyguard.ui.viewmodel.OccludedToLockscreenTransitionViewModel;
 import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToGoneTransitionViewModel;
+import com.android.systemui.derp.pulselight.PulseLightView;
 import com.android.systemui.media.controls.pipeline.MediaDataManager;
 import com.android.systemui.media.controls.ui.KeyguardMediaController;
 import com.android.systemui.media.controls.ui.MediaHierarchyManager;
@@ -206,6 +207,7 @@ import com.android.systemui.statusbar.notification.NotificationWakeUpCoordinator
 import com.android.systemui.statusbar.notification.PropertyAnimator;
 import com.android.systemui.statusbar.notification.ViewGroupFadeHelper;
 import com.android.systemui.statusbar.notification.collection.NotificationEntry;
+import com.android.systemui.statusbar.notification.collection.NotifPipeline;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.notification.row.ExpandableView;
 import com.android.systemui.statusbar.notification.row.NotificationGutsManager;
@@ -744,6 +746,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     private final ActivityStarter mActivityStarter;
 
+    private PulseLightView mPulseLightView;
+    private NotifPipeline mNotifPipeline;
+
     @Inject
     public NotificationPanelViewController(NotificationPanelView view,
             @Main Handler handler,
@@ -832,7 +837,8 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             KeyguardFaceAuthInteractor keyguardFaceAuthInteractor,
             KeyguardRootView keyguardRootView,
             TunerService tunerService,
-            Context context) {
+            Context context,
+            NotifPipeline notifPipeline) {
         keyguardStateController.addCallback(new KeyguardStateController.Callback() {
             @Override
             public void onKeyguardFadingAwayChanged() {
@@ -1058,6 +1064,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mAlternateBouncerInteractor = alternateBouncerInteractor;
         mKeyguardRootView = keyguardRootView;
         dumpManager.registerDumpable(this);
+        mNotifPipeline = notifPipeline;
         mLocalPowerManager = LocalServices.getService(PowerManagerInternal.class);
         mPerf = new BoostFramework();
     }
@@ -1150,6 +1157,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (!mFeatureFlags.isEnabled(Flags.MIGRATE_SPLIT_KEYGUARD_BOTTOM_AREA)) {
             setKeyguardBottomArea(mView.findViewById(R.id.keyguard_bottom_area));
         }
+        mPulseLightView = (PulseLightView) mView.findViewById(R.id.pulse_light_view);
 
         mReTickerComeback = mView.findViewById(R.id.ticker_comeback);
         mReTickerComebackIcon = mView.findViewById(R.id.ticker_comeback_icon);
@@ -3270,9 +3278,42 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (!mPulsing && !mDozing) {
             mAnimateNextPositionUpdate = false;
         }
+
+        showPulseLight();
+
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
 
         updateKeyguardStatusViewAlignment(/* animate= */ true);
+    }
+
+    private void showPulseLight() {
+        if (mPulseLightView == null || !isPulseLightEnabled()) return;
+        if (mPulsing) {
+            // Get the notification that's pulsing
+            String notifPackageName = "";
+            List<NotificationEntry> notificationEntries =
+                    new ArrayList(mNotifPipeline.getAllNotifs());
+            for (int i = 0; i < notificationEntries.size(); i++) {
+                NotificationEntry entry = notificationEntries.get(i);
+                if (entry.showingPulseLight()) {
+                    notifPackageName = entry.getSbn().getPackageName();
+                    break;
+                }
+            }
+            // Animate edge light only for notification pulse.
+            // Package not empty means pulse caused by a notification.
+            if (!notifPackageName.isEmpty()) {
+                mPulseLightView.setVisibility(View.VISIBLE);
+                mPulseLightView.animateNotification(notifPackageName);
+            }
+        } else {
+            mPulseLightView.setVisibility(View.GONE);
+        }
+    }
+
+    private boolean isPulseLightEnabled() {
+        return Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
+                Settings.Secure.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
     }
 
     public void setAmbientIndicationBottomPadding(int bottomPadding) {

packages/SystemUI/src/com/android/systemui/statusbar/notification/collection/coordinator/PulseLightCoordinator.kt
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2023 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.statusbar.notification.collection.coordinator
+
+import android.app.Notification
+
+import com.android.systemui.derp.pulselight.PulseLightNotifManager
+import com.android.systemui.statusbar.notification.collection.NotifPipeline
+import com.android.systemui.statusbar.notification.collection.NotificationEntry
+import com.android.systemui.statusbar.notification.collection.coordinator.dagger.CoordinatorScope
+import com.android.systemui.statusbar.notification.collection.notifcollection.NotifCollectionListener
+
+import javax.inject.Inject
+
+@CoordinatorScope
+class PulseLightCoordinator @Inject constructor(
+    private val manager: PulseLightNotifManager
+) : Coordinator {
+
+    private val notifCollectionListener = object : NotifCollectionListener {
+
+        override fun onEntryAdded(entry: NotificationEntry) {
+            manager.onNotificationPosted(entry)
+        }
+
+        override fun onEntryUpdated(entry: NotificationEntry) {
+            // Pass notification only if it's have to alert user again.
+            if (shouldAlertAgain(entry)) {
+                manager.onNotificationPosted(entry)
+            }
+        }
+
+    }
+
+    /**
+     * Checks whether an update for a notification warrants an alert for the user.
+     */
+    private fun shouldAlertAgain(entry: NotificationEntry): Boolean {
+        return (!entry.hasInterrupted() ||
+                (entry.sbn.notification.flags and Notification.FLAG_ONLY_ALERT_ONCE) == 0)
+    }
+
+
+    override fun attach(pipeline: NotifPipeline) {
+        pipeline.addCollectionListener(notifCollectionListener)
+    }
+
+}